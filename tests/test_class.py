49f19b8024c9a4d5934365c6471ae7f322e9310d
Шляпин Илья <shliapin.ilia@yandex.ru>
2021-05-24 23:00:45 +0300

    Added test script to test out new structure
    Signed-off-by: Шляпин Илья <shliapin.ilia@yandex.ru>

---
+++ tests/test_class.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+import sys
+sys.path.append(os.path.normpath(os.path.join
+                (os.path.dirname(os.path.abspath(__file__)), '..')))
+
+from src.game import Game
+
+WIDTH = 800
+HEIGHT = 650
+FPS = 30
+
+play = Game(WIDTH, HEIGHT, FPS)
+play.mainloop()
+play.quit()
+
