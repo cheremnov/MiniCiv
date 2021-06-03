5438509d9c179dae286c4f5b79735cf103fef6c1
TrickyFox371 <chernokni@ya.ru>
2021-06-03 18:07:47 +0300

    Add documentation to vis_cursor class.
    Signed-off-by: TrickyFox371 <chernokni@ya.ru>

--- src/visual/vis_cursor.py
+++ src/visual/vis_cursor.py
@@ -1,3 +1,4 @@
+"""Visual cursor."""
 import pygame
 from src.visual.vis_object import vis_object
 
@@ -8,10 +9,19 @@ HEIGHT = 650
 
 
 class vis_cursor(vis_object):
+    """Represent game cursor."""
+
     def __init__(self, cursor_img):
+        """Init game cursor.
+
+        Args:
+            - cursor_img - cursor image
+
+        """
         vis_object.__init__(self, WIDTH / 2, HEIGHT / 2, cursor_img)
         self._layer = 5
 
     def update(self):
+        """Update cursor location."""
         x, y = pygame.mouse.get_pos()
         self.rect.topleft = (x, y)
