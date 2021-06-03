276f28a28eee5b6f3be858a81e8af5ea44b3ddf1
TrickyFox371 <chernokni@ya.ru>
2021-06-03 18:00:49 +0300

    Add documentation to vis_frame class.
    Signed-off-by: TrickyFox371 <chernokni@ya.ru>

--- src/visual/vis_frame.py
+++ src/visual/vis_frame.py
@@ -1,3 +1,4 @@
+"""Visual frame."""
 from src.visual.vis_object import vis_object
 import pygame.freetype
 
@@ -6,21 +7,63 @@ WHITE = (255, 255, 255)
 
 
 class vis_frame(vis_object):
+    """Represents game frame."""
+
     def __init__(self, x, y, frame_img, game_map):
+        """Initialise vis_frame.
+
+        Args:
+            - x - x coordinate
+            - y - y coordinate
+            - frame_img - frame image
+            - game_map - game map
+
+        """
         vis_object.__init__(self, x, y, frame_img)
         self.map = game_map
         self._layer = 3
 
     def check_right_click(self, mouse, master=None):
+        """React on right mouse button click.
+
+        Args:
+            - mouse - mouse click info
+            - master - current game
+
+        """
         self.map.set_moving(True)
 
     def check_right_release(self, mouse, master=None):
+        """React on right mouse button release.
+
+        Args:
+            - mouse - mouse click info
+            - master - current game
+
+        """
         self.map.set_moving(False)
 
     def cell_center(self, x, y):
+        """Get cell's center.
+
+        Args:
+            - x - cell's x coordinate
+            - y - cell's y coordinate
+
+        Returns:
+            - output - cell's center
+
+        """
         return self.map.get_cells()[x][y].vis_cell.rect.center
 
     def check_motion(self, rel, master=None):
+        """React on mouse motion.
+
+        Args:
+            - rel - mouse click info
+            - master - current game
+
+        """
         move_allowed = True
         if rel[0] > 0 and self.cell_center(0, 0)[0] > self.rect.center[0]:
             move_allowed = False
@@ -46,10 +89,16 @@ class vis_frame(vis_object):
             self.map.move(rel)
 
     def set_text(self, text, color):
-        '''
+        """Set text on frame.
+
         Render the text over the center of frame.
         It is used to write defeat messages.
-        '''
+
+        Args:
+            - text - text, that will be set
+            - color - text color
+
+        """
         coords = (120, 325)
         font = pygame.freetype.SysFont('Comic Sans MS', 96)
         linestep = font.get_sized_height()
