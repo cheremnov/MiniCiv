778a03d749fa775c1cb6c27d450adf67d27db822
TrickyFox371 <chernokni@ya.ru>
2021-06-03 18:34:30 +0300

    Add documentation to cells directory.
    Signed-off-by: TrickyFox371 <chernokni@ya.ru>

--- src/cells/plain_cell.py
+++ src/cells/plain_cell.py
@@ -1,7 +1,16 @@
+"""Plain cell."""
 from src.cell import Cell
 
 
 class Plain_cell(Cell):
+    """Represent plain cell."""
 
     def __init__(self, x, y):
+        """Initialise Plain_cell (in work).
+
+        Args:
+            - x - x coordinate
+            - y - y coordinate
+
+        """
         super().__init__(x, y)
--- src/cells/sea_cell.py
+++ src/cells/sea_cell.py
@@ -1,7 +1,16 @@
+"""Sea cell."""
 from src.cell import Cell
 
 
 class Sea_cell(Cell):
+    """Represent sea cell."""
 
     def __init__(self, x, y):
+        """Initialise Sea_cell (in work).
+
+        Args:
+            - x - x coordinate
+            - y - y coordinate
+
+        """
         super().__init__(x, y)
